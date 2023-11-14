fd9fa5454d47c60e41792c65beccf3988f3f2225
Brett <brettmaster1@gmail.com>
2023-11-14 01:05:28 -0500

    compiler support for blackbox

--- include/blt/std/assert.h
+++ include/blt/std/assert.h
@@ -14,6 +14,42 @@ namespace blt
     void b_assert_failed(const char* expression, const char* path, int line);
     
     void b_throw(const char* what, const char* path, int line);
+
+#if defined(__GNUC__) || defined(__llvm__)
+    #define BLT_ATTRIB_NO_INLINE __attribute__ ((noinline))
+#else
+    #if defined(_MSC_VER) && !defined(__INTEL_COMPILER)
+        #define BLT_ATTRIB_NO_INLINE __declspec(noinline)
+    #else
+        #define BLT_ATTRIB_NO_INLINE
+    #endif
+#endif
+    
+    template<typename T>
+    void BLT_ATTRIB_NO_INLINE black_box_ref(const T& val){
+        volatile void* hell;
+        hell = (void*)&val;
+    }
+    
+    template<typename T>
+    void BLT_ATTRIB_NO_INLINE black_box(T val){
+        volatile void* hell2;
+        hell2 = (void*)&val;
+    }
+    
+    template<typename T>
+    const T& BLT_ATTRIB_NO_INLINE black_box_ref_ret(const T& val){
+        volatile void* hell;
+        hell = (void*)&val;
+        return val;
+    }
+    
+    template<typename T>
+    T BLT_ATTRIB_NO_INLINE black_box_ret(T val){
+        volatile void* hell2;
+        hell2 = (void*)&val;
+        return val;
+    }
 }
 
 // prints error with stack trace if assertion fails. Does not stop execution.
